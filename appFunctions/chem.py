d399ab3f1b1c6b9d6c040e9d1cf7e1e697b99baa
Kubaczlollol <121460703+Kubaczlollol@users.noreply.github.com>
2022-12-30 15:33:20 +0100

    removed print statementsa nd text from input functions and added it to comments

--- appFunctions/chem.py
+++ appFunctions/chem.py
@@ -1,67 +1,61 @@
 # Converting between units (ml, l)
 def volume_unit(volume_number):
     while True:
-        volume_unit: str = input("Zadana hodnota je v (l)litrech nebo (m)mililitrech?: ").lower()
-        if volume_unit == "m":
+        volume_unit: str = input().lower() #Zadana hodnota je v (l)litrech nebo (m)mililitrech?
+        if volume_unit == "m": #Konverze z ml na l
             volume_number /= 1000
-            print(volume_number, "l")
             return volume_number
-        if volume_unit == "l":
-            print(volume_number, "l")
+        if volume_unit == "l": #Konverze z l na l (zadna koverze)
             return volume_number
-        print("Chyba ve vyberu l/ml, zadejte (l) pro litry nebo (m) pro mililitry: ")
-
-
-# Calling all necessary functions and calculating the molar concentration from volume, molar concentration and volume
-def volume_standartization(ratio_x_standartizanec=1, ratio_y_standartizatel=1):
-    volume_1 = float(input("Zadejte spotrebu standardizacniho roztoku: "))
-    volume_1 = volume_unit(volume_1)
-
-    concentration_1 = float(input("Zadejte koncentrace standartizacniho roztoku v mol/l: "))
-    print(concentration_1, "mol/l")
-
-    volume_2 = float(input("Zadejte objem merene latky: "))
-    volume_2 = volume_unit(volume_2)
-
-    concentration_f = concentration_1*volume_1*ratio_y_standartizatel/(volume_2*ratio_x_standartizanec)
-
-    print(f"Koncentrace mereneho roztoku je {concentration_f} mol/l")
-
+        #Hodnota musi byt l nebo ml
 
 # Converting between units (mg, g, kg)
 def weight_unit(m):
     while True:
-        m_unit: str = input("Zadana hodnota je v (k)ilech, (g)ramech nebo v (m)iligramech?: ").lower()
-        if m_unit == "k":
+        m_unit: str = input().lower() #Zadana hodnota je v (k)ilech, (g)ramech nebo v (m)iligramech?
+        if m_unit == "k": #koverze z kilogramu na gram
             m *= 1000
-            print(m, "g")
             return m
-        if m_unit == "g":
-            print(m, "g")
+        if m_unit == "g": #konverze z gramu na gram (zadna koverze)
             return m
-        if m_unit == "m":
+        if m_unit == "m": #konverze z miligramu na gram
             m /= 1000
-            print(m, "g")
             return m
-        print("Chyba ve vyberu kg/g/mg")
+        #Hodnota musi byt k, g nebo m
+
+
+# Calling all necessary functions and calculating the molar concentration from volume, molar concentration and volume
+def volume_standartization(ratio_zakladni_latky=1, ratio_odmerneho_roztoku=1):
+    volume_1 = float(input()) #Zadejte spotrebu standardizacniho roztoku
+    volume_1 = volume_unit(volume_1)
+
+    concentration_1 = float(input()) #Zadejte koncentrace standartizacniho roztoku v mol/l
+
+    volume_2 = float(input()) #Zadejte objem merene latky
+    volume_2 = volume_unit(volume_2) 
+
+    return(
+        concentration_1*volume_1*ratio_odmerneho_roztoku/(volume_2*ratio_zakladni_latky)
+    )
+
 
 
 # Calling the weight_unit function and Calculating amount of substance from molar weight nad weight
 def amount_of_substance():
-    m1 = float(input("Zadejte hmotnost latky: "))
+    m1 = float(input()) #Zadejte hmotnost latky
     m1 = weight_unit(m1)
 
-    mw1 = float(input("Zadejte molarni hmotnost latky: "))
+    mw1 = float(input()) #Zadejte molarni hmotnost latky
 
-    n = m1/mw1
-    print(n, "molu")
-    return n
+    n = m1/mw1 
+    return n #Výsledek je v molech jen pokud byla yadana hodnota v gramech
 
 
 # Calling all necessary functions and calculating the amount of substance and volume
-def weight_standartization(ratio_x_standartizanec=1, ratio_y_standartizatel=1):
+def weight_standartization(ratio_zakladni_latky=1, ratio_odmerneho_roztoku=1):
     n = amount_of_substance()
-    v = float(input("Zadejte objem merene latky: "))
-    v = volume_unit(v)/(v*ratio_x_standartizanec)
-    c = n*ratio_y_standartizatel/(v*ratio_x_standartizanec)
-    print(f"Koncentrace mereneho roztoku je {c} mol/l")
+    v : float = float(input()) #Zadejte objem merene latky
+    v = volume_unit(v)
+    return( 
+        n*ratio_odmerneho_roztoku/(v*ratio_zakladni_latky)
+        ) 
